4634bd52bcfd869e341fed6f8b0c5dabd8cce3db
JohnnyJianHY <JohnnyJianHY@07f37d2e-f84a-0410-9487-6fce810d0c13>
2008-10-11 14:54:57 +0000

    Introduce the GMockController class and refactor the GMockTestCase class - issue 5

--- trunk/src/main/groovy/org/gmock/GMockTestCase.groovy
+++ trunk/src/main/groovy/org/gmock/GMockTestCase.groovy
@@ -2,38 +2,10 @@ package org.gmock
 
 abstract class GMockTestCase extends GroovyTestCase {
 
-    def mocks = []
+    def gMockController = new GMockController()
 
-    ProxyCollection proxyCollection = new ProxyCollection()
+    def mock = gMockController.&mock
 
-    def mock(){
-        def mock = new Mock()
-        mocks.add(mock)
-        return mock
-    }
-
-    def mock(Map constraints, Class aClass){
-        def mock = mock()
-        if (constraints.constructor != null){
-            proxyCollection.expectConstructor(aClass, constraints.constructor, mock)
-        }
-        return mock
-    }
-
-
-
-    def play = { closure ->
-        mocks.each {it._replay()}
-        proxyCollection.startProxy()
-        try {
-            closure.call()
-        } finally {
-            proxyCollection.stopProxy()
-        }
-        mocks.each {it._verify()}
-        proxyCollection.verify()
-        mocks.each {it._reset()}
-    }
-
+    def play = gMockController.&play
 
 }
