566e83c199c8b5cf1a6f8a73f0862f4493e19343
JohnnyJianHY <JohnnyJianHY@07f37d2e-f84a-0410-9487-6fce810d0c13>
2008-10-15 08:50:10 +0000

    More tests on signature classes

---
+++ trunk/src/test/groovy/org/gmock/internal/signature/StaticSignatureTest.groovy
@@ -0,0 +1,43 @@
+package org.gmock.internal.signature
+
+import org.gmock.Loader
+import org.gmock.Mock
+
+class StaticSignatureTest extends GroovyTestCase {
+
+    void testEquals() {
+        def signature1 = new StaticSignature(Loader, "put", [1, 2])
+        def signature2 = new StaticSignature(Loader, "put", [1, 2])
+        assertEquals signature1, signature2
+    }
+
+    void testNotEqualsDifferentClass() {
+        def signature1 = new StaticSignature(Loader, "put", [1, 2])
+        def signature2 = new StaticSignature(Mock, "put", [1, 2])
+        assert signature1 != signature2
+    }
+
+    void testNotEqualsDifferentMethodName() {
+        def signature1 = new StaticSignature(Loader, "put", [1, 2])
+        def signature2 = new StaticSignature(Loader, "get", [1, 2])
+        assert signature1 != signature2
+    }
+
+    void testNotEqualsDifferentParameters() {
+        def signature1 = new StaticSignature(Loader, "put", [1, 2])
+        def signature2 = new StaticSignature(Loader, "put", [3, 4])
+        assert signature1 != signature2
+    }
+
+    void testHashCode() {
+        def signature1 = new StaticSignature(Loader, "put", [1, 2])
+        def signature2 = new StaticSignature(Loader, "put", [1, 2])
+        assertEquals signature1.hashCode(), signature2.hashCode()
+    }
+
+    void testToString() {
+        def signature = new StaticSignature(Loader, "put", ["test", 3, true])
+        assertEquals "Loader.put(test,3,true)", signature.toString()
+    }
+
+}
