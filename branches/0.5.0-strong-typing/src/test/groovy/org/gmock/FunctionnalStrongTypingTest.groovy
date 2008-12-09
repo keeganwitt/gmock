cf04c7cbd5e34d5b4c733e3f0e91f5fec3ed3fb2
julien.gagnet <julien.gagnet@07f37d2e-f84a-0410-9487-6fce810d0c13>
2008-12-09 19:23:54 +0000

    Added more tests for strong typing - issue 14

--- branches/0.5.0-strong-typing/src/test/groovy/org/gmock/FunctionnalStrongTypingTest.groovy
+++ branches/0.5.0-strong-typing/src/test/groovy/org/gmock/FunctionnalStrongTypingTest.groovy
@@ -1,5 +1,8 @@
 package org.gmock
 
+import org.gmock.utils.GroovyFinalClass
+import org.codehaus.groovy.tools.GroovyClass
+
 
 class FunctionnalStrongTypingTest extends GMockTestCase {
 
@@ -12,6 +15,67 @@ class FunctionnalStrongTypingTest extends GMockTestCase {
         play {
             assertEquals("a value", cache.load("a key"))
             assertEquals("a value", cache.load("a key"))
+        }
+
+    }
+
+
+    void testGroovyClass(){
+        GroovyClass mockGroovy = mock(GroovyClass)
+        mockGroovy.aMethod("args").returns("called")
+        play {
+            assertEquals("called", mockGroovy.aMethod("args"))
+        }
+    }
+
+    void testMatchConstructor(){
+        Loader mockLoader = mock(Loader, constructor: ["a name"])
+        mockLoader.load("key").returns("value")
+        play {
+            Loader loader = new Loader("a name")
+            assertEquals "a value", loader.load("key")
+        }
+    }
+
+    void testInterface(){
+        List list = mock(List)
+        list.size().returns(10)
+        play {
+            assertEquals(10, list.size())
+        }
+    }
+
+    void testJavaFinalClass(){
+        String mockJavaFinal = mock(String)
+        mockJavaFinal.startsWith("ham").returns(true)
+        play {
+            assertTrue mockJavaFinal.startsWith("ham")
+        }
+    }
+
+    void testGroovyFinalClass(){
+        GroovyFinalClass mockGroovyFinal = mock(GroovyFinalClass)
+        mockGroovyFinal.aMethod("args").returns("called")
+        play {
+            assertEquals("called", mockGroovyFinal.aMethod("args"))
+        }
+    }
+
+    void multiplePlay(){
+        JavaLoader mockLoader = mock(JavaLoader)
+
+        JavaCache cache = new JavaCache(mockLoader)
+        mockLoader.load("a key").returns("a value").once()
+
+        play {
+            assertEquals("a value", cache.load("a key"))
+            assertEquals("a value", cache.load("a key"))
+        }
+        
+        mockLoader.load("another key").returns("another value").once()
+        play {
+            assertEquals("another value", cache.load("another key"))
+            assertEquals("another value", cache.load("another key"))
         }
 
     }
--- branches/0.5.0-strong-typing/src/test/java/org/gmock/JavaLoader.java
+++ branches/0.5.0-strong-typing/src/test/java/org/gmock/JavaLoader.java
@@ -2,6 +2,12 @@ package org.gmock;
 
 public class JavaLoader {
 
+    private String name;
+
+    public JavaLoader(String name){
+        this.name = name;
+    }
+
     public String load(String key){
         return "key";
     }
