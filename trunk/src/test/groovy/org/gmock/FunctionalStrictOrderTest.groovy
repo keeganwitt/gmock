5e78e4f72663e0414cf5fe66f7346cdb8f520971
JohnnyJianHY <JohnnyJianHY@07f37d2e-f84a-0410-9487-6fce810d0c13>
2009-02-22 07:17:58 +0000

    Support multiple strict closures - issue 50

--- trunk/src/test/groovy/org/gmock/FunctionalStrictOrderTest.groovy
+++ trunk/src/test/groovy/org/gmock/FunctionalStrictOrderTest.groovy
@@ -31,6 +31,24 @@ class FunctionalStrictOrderTest extends GMockTestCase {
         }
     }
 
+    void testOneStrictClosureWithOneMockAndUnorderedExpectations() {
+        def mock = mock()
+        strict {
+            mock.a().returns(1)
+            mock.b().returns(2)
+            mock.c().returns(3)
+        }
+        mock.b().returns(4)
+        mock.d().returns(5)
+        play {
+            assertEquals 5, mock.d()
+            assertEquals 1, mock.a()
+            assertEquals 2, mock.b()
+            assertEquals 4, mock.b()
+            assertEquals 3, mock.c()
+        }
+    }
+
     void testOneStrictClosureWithOneMockAndFailed() {
         def mock = mock()
         strict {
@@ -47,18 +65,19 @@ class FunctionalStrictOrderTest extends GMockTestCase {
         }
     }
 
-    void testStrictClosureWithTimesExpectation() {
+    void testStrictClosureWithTimesExpectations() {
         def mock = mock()
         strict {
-            mock.a().times(2)
-            mock.a().times(1..3)
-            mock.b().stub()
-            mock.c().atLeast(2)
-            mock.d().never()
+            mock.a().returns(1).times(2)
+            mock.a().returns(2).times(1..3)
+            mock.b().returns(3).stub()
+            mock.c().returns(4).atLeast(2)
+            mock.d().returns(5).never()
         }
         play {
-            4.times { mock.a() }
-            3.times { mock.c() }
+            2.times { assertEquals 1, mock.a() }
+            2.times { assertEquals 2, mock.a() }
+            3.times { assertEquals 4, mock.c() }
         }
     }
 
@@ -115,6 +134,30 @@ class FunctionalStrictOrderTest extends GMockTestCase {
         }
     }
 
+    void testOneStrictClosureWithMultiMocksAndUnorderedExpectations() {
+        def m1 = mock(), m2 = mock(), m3 = mock()
+        m1.a().returns(5)
+        m1.b().returns(6)
+        strict {
+            m1.a().returns(1)
+            m2.b().returns(2)
+            m3.b().returns(3)
+            m1.c().returns(4)
+        }
+        m3.b().returns(7)
+        m1.c().returns(8)
+        play {
+            assertEquals 1, m1.a()
+            assertEquals 6, m1.b()
+            assertEquals 2, m2.b()
+            assertEquals 5, m1.a()
+            assertEquals 3, m3.b()
+            assertEquals 7, m3.b()
+            assertEquals 4, m1.c()
+            assertEquals 8, m1.c()
+        }
+    }
+
     void testOneStrictClosureWithMultiMocksAndFailed() {
         def m1 = mock(), m2 = mock(), m3 = mock()
         strict {
@@ -130,6 +173,162 @@ class FunctionalStrictOrderTest extends GMockTestCase {
                 m2.b()
                 m1.c()
             }
+        }
+    }
+
+    void testTwoStrictClosuresWithOneMock() {
+        def mock = mock()
+        def setUp = { ->
+            strict {
+                mock.a().returns(1)
+                mock.b().returns(2)
+                mock.c().returns(3)
+            }
+            strict {
+                mock.a().returns(4)
+                mock.c().returns(5)
+                mock.d().returns(6)
+            }
+        }
+        setUp()
+        play {
+            assertEquals 1, mock.a()
+            assertEquals 2, mock.b()
+            assertEquals 3, mock.c()
+            assertEquals 4, mock.a()
+            assertEquals 5, mock.c()
+            assertEquals 6, mock.d()
+        }
+
+        setUp()
+        play {
+            assertEquals 1, mock.a()
+            assertEquals 4, mock.a()
+            assertEquals 5, mock.c()
+            assertEquals 2, mock.b()
+            assertEquals 6, mock.d()
+            assertEquals 3, mock.c()
+        }
+    }
+
+    void testTwoStrictClosuresWithOneMockAndFailed() {
+        def mock = mock()
+        strict {
+            mock.a()
+            mock.b()
+        }
+        strict {
+            mock.a()
+            mock.c()
+        }
+        shouldFail { // TODO: check the message
+            play {
+                mock.a()
+                mock.c()
+                mock.a()
+                mock.b()
+            }
+        }
+    }
+
+    void testTwoStrictClosuresWithOneMockAndTimesExpectations() {
+        def mock = mock()
+        strict {
+            mock.a().returns(1).times(2..3)
+            mock.b().returns(2).times(2)
+        }
+        mock.b().returns(3).atLeast(2)
+        strict {
+            mock.b().returns(4)
+            mock.c().returns(5).atMost(3)
+        }
+        play {
+            assertEquals 4, mock.b()
+            2.times { assertEquals 3, mock.b() }
+            3.times { assertEquals 1, mock.a() }
+            2.times { assertEquals 2, mock.b() }
+            assertEquals 5, mock.c()
+        }
+    }
+
+    void testThreeStrictClosuresWithOneMockAndUnorderedExpectations() {
+        def mock = mock()
+        mock.a().returns(0)
+        strict {
+            mock.a().returns(1)
+            mock.b().returns(2)
+        }
+        mock.b().returns(3)
+        strict {
+            mock.a().returns(4)
+            mock.c().returns(5)
+            mock.d().returns(6)
+        }
+        strict {
+            mock.b().returns(7)
+            mock.d().returns(8)
+        }
+        mock.e().returns(9)
+        play {
+            assertEquals 1, mock.a()
+            assertEquals 4, mock.a()
+            assertEquals 9, mock.e()
+            assertEquals 2, mock.b()
+            assertEquals 0, mock.a()
+            assertEquals 5, mock.c()
+            assertEquals 7, mock.b()
+            assertEquals 6, mock.d()
+            assertEquals 8, mock.d()
+            assertEquals 3, mock.b()
+        }
+    }
+
+    void testMultiStrictClosuresWithMultiMocksAndOneForEach() {
+        def m1 = mock(), m2 = mock(), m3 = mock()
+        strict {
+            m1.a().returns(1)
+            m1.b().returns(2)
+        }
+        strict {
+            m2.a().returns(3)
+            m2.b().returns(4)
+        }
+        strict {
+            m3.a().returns(5)
+            m3.c().returns(6)
+        }
+        play {
+            assertEquals 5, m3.a()
+            assertEquals 3, m2.a()
+            assertEquals 1, m1.a()
+            assertEquals 2, m1.b()
+            assertEquals 4, m2.b()
+            assertEquals 6, m3.c()
+        }
+    }
+
+    void testTwoStrictClosuresWithThreeMocks() {
+        def m1 = mock(), m2 = mock(), m3 = mock()
+        strict {
+            m1.a().returns(1)
+            m2.b().returns(2)
+            m3.c().returns(3)
+        }
+        strict {
+            m2.b().returns(4)
+            m1.a().returns(5)
+            m3.d().returns(6)
+        }
+        m1.a().returns(7).stub()
+        play {
+            assertEquals 4, m2.b()
+            assertEquals 1, m1.a()
+            assertEquals 5, m1.a()
+            assertEquals 7, m1.a()
+            assertEquals 2, m2.b()
+            assertEquals 3, m3.c()
+            assertEquals 6, m3.d()
+            assertEquals 7, m1.a()
         }
     }
 
