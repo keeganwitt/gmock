d1b45bfd762707c9268182b50d036d99fcfd6134
JohnnyJianHY <JohnnyJianHY@07f37d2e-f84a-0410-9487-6fce810d0c13>
2009-03-31 06:46:20 +0000

    Added a collaborator sample which is brought from EasyMock and is changed to use testng

---
+++ trunk/samples/collaborator/src/test/groovy/org/gmock/sample/ExampleTest.groovy
@@ -0,0 +1,104 @@
+package org.gmock.sample
+
+import org.testng.annotations.Test
+import org.testng.annotations.BeforeMethod
+import org.gmock.WithGMock
+
+@WithGMock
+class ExampleTest {
+
+    ClassUnderTest classUnderTest
+    Collaborator mock
+
+    @BeforeMethod void setup() {
+        mock = mock(Collaborator)
+        classUnderTest = new ClassUnderTest()
+//        classUnderTest.addListener(mock)
+    }
+
+    @Test void removeNonExistingDocument() {
+        play {
+            classUnderTest.addListener(mock) // TODO: move to setup()
+            classUnderTest.removeDocument("Does not exist")
+        }
+    }
+
+    @Test void addDocument() {
+        mock.documentAdded("New Document")
+        play {
+            classUnderTest.addListener(mock) // TODO: move to setup()
+            classUnderTest.addDocument("New Document", new byte[0])
+        }
+    }
+
+    @Test void addAndChangeDocument() {
+        mock.documentAdded("Document")
+        mock.documentChanged("Document").times(3)
+        play {
+            classUnderTest.addListener(mock) // TODO: move to setup()
+            4.times {
+                classUnderTest.addDocument("Document", new byte[0])
+            }
+        }
+    }
+
+    @Test void voteForRemoval() {
+        // expect document addition
+        mock.documentAdded("Document")
+        // expect to be asked to vote, and vote for it
+        mock.voteForRemoval("Document").returns(42)
+        // expect document removal
+        mock.documentRemoved("Document")
+
+        play {
+            classUnderTest.addListener(mock) // TODO: move to setup()
+            classUnderTest.addDocument("Document", new byte[0])
+            assert classUnderTest.removeDocument("Document")
+        }
+    }
+
+    @Test void voteAgainstRemoval() {
+        // expect document addition
+        mock.documentAdded("Document")
+        // expect to be asked to vote, and vote against it
+        mock.voteForRemoval("Document").returns(-42)
+        // document removal is *not* expected
+
+        play {
+            classUnderTest.addListener(mock) // TODO: move to setup()
+            classUnderTest.addDocument("Document", new byte[0])
+            assert !classUnderTest.removeDocument("Document")
+        }
+    }
+
+    @Test void voteForRemovals() {
+        with(mock) {
+            documentAdded("Document 1")
+            documentAdded("Document 2")
+            voteForRemovals(["Document 1", "Document 2"] as String[]).returns(42)
+            documentRemoved("Document 1")
+            documentRemoved("Document 2")
+        }
+        play {
+            classUnderTest.addListener(mock) // TODO: move to setup()
+            classUnderTest.addDocument("Document 1", new byte[0])
+            classUnderTest.addDocument("Document 2", new byte[0])
+            assert classUnderTest.removeDocuments("Document 1", "Document 2")
+        }
+    }
+
+    @Test void voteAgainstRemovals() {
+        with(mock) {
+            documentAdded("Document 1")
+            documentAdded("Document 2")
+            voteForRemovals(["Document 1", "Document 2"] as String[]).returns(-42)
+        }
+        play {
+            classUnderTest.addListener(mock) // TODO: move to setup()
+            classUnderTest.addDocument("Document 1", new byte[0])
+            classUnderTest.addDocument("Document 2", new byte[0])
+            assert !classUnderTest.removeDocuments("Document 1", "Document 2")
+        }
+    }
+
+}
